7f54024a4bd20095b4c6ce7df70b41812f5fbcff
Gayrat Rakhimov <rakhimov.gayrat@gmail.com>
2022-05-08 15:06:39 +0500

    Location using coroutines

--- app/src/main/java/com/adyen/android/assignment/ui/places/PlacesFragment.kt
+++ app/src/main/java/com/adyen/android/assignment/ui/places/PlacesFragment.kt
@@ -1,11 +1,9 @@
 package com.adyen.android.assignment.ui.places
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,12 +17,12 @@ import com.adyen.android.assignment.api.model.Place
 import com.adyen.android.assignment.ui.base.RecyclerClickListener
 import com.adyen.android.assignment.extensions.hide
 import com.adyen.android.assignment.extensions.show
-import com.google.android.gms.location.*
-import com.google.android.gms.tasks.CancellationTokenSource
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.places_fragment.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @AndroidEntryPoint
+@ExperimentalCoroutinesApi
 class PlacesFragment : Fragment() {
 
     companion object {
@@ -35,8 +33,8 @@ class PlacesFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        init()
-        if(savedInstanceState==null) checkLocationPermission()
+        if (savedInstanceState == null) checkLocationPermission()
+        observeData()
     }
 
     override fun onCreateView(
@@ -46,16 +44,17 @@ class PlacesFragment : Fragment() {
         return inflater.inflate(R.layout.places_fragment, container, false)
     }
 
-    private fun init() {
+    private fun observeData() {
         lifecycleScope.launchWhenStarted {
             viewModel.loading.collect { loading ->
                 if (loading) {
                     progress_loading.show()
-                    text_message.show()
-                    text_message.text = getString(R.string.loading_data)
+                    text_loading.show()
+                    text_loading.text = getString(R.string.loading)
+                    button_try_again.hide()
                 } else {
                     progress_loading.hide()
-                    text_message.hide()
+                    text_loading.hide()
                 }
             }
         }
@@ -76,71 +75,47 @@ class PlacesFragment : Fragment() {
             viewModel.error.collect { error ->
                 if (error) {
                     progress_loading.hide()
-                    text_message.show()
-                    text_message.text = getString(R.string.something_went_wrong)
+                    text_loading.show()
+                    text_loading.text = getString(R.string.something_went_wrong)
                     button_try_again.show()
                     button_try_again.setOnClickListener {
-                        text_message.hide()
+                        text_loading.hide()
                         button_try_again.hide()
-                        getLocation()
+                        checkLocationPermission()
+                    }
+                }
+            }
+        }
+        lifecycleScope.launchWhenStarted {
+            viewModel.locationPermissionGranted.collect { isGranted ->
+                if (!isGranted) {
+                    recycler_places.hide()
+                    progress_loading.hide()
+                    text_loading.show()
+                    text_loading.text = getString(R.string.location_permission_is_not_granted)
+                    button_try_again.show()
+                    button_try_again.setOnClickListener {
+                        checkLocationPermission()
                     }
                 }
             }
         }
     }
 
-
     private val requestPermissionLauncher =
         registerForActivityResult(
             ActivityResultContracts.RequestPermission()
         ) { isGranted: Boolean ->
             if (isGranted) {
-                progress_loading.hide()
-                text_message.hide()
-                button_location_permission.hide()
-                getLocation()
+                viewModel.onLocationPermissionChange(true)
+                viewModel.getLocation()
             } else {
-                recycler_places.hide()
-                progress_loading.hide()
-                text_message.show()
-                text_message.text = getString(R.string.location_permission_is_not_granted)
-                button_location_permission.show()
-                button_location_permission.setOnClickListener {
-                    checkLocationPermission()
-                }
+                viewModel.onLocationPermissionChange(false)
             }
         }
 
     private fun checkLocationPermission() {
         requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun getLocation() {
-        lifecycleScope.launchWhenStarted {
-            progress_loading.show()
-            text_message.show()
-            text_message.text = getString(R.string.retrieving_location)
-            button_location_permission.hide()
-            val fusedLocationProviderClient = activity?.let {
-                LocationServices.getFusedLocationProviderClient(it)
-            }
-            val cts = CancellationTokenSource()
-            fusedLocationProviderClient?.getCurrentLocation(
-                LocationRequest.PRIORITY_HIGH_ACCURACY,
-                cts.token
-            )?.addOnSuccessListener {
-                progress_loading.hide()
-                text_message.hide()
-                val latitude = it.latitude
-                val longitude = it.longitude
-                getPlaces(latitude, longitude)
-            }
-        }
-    }
-
-    private fun getPlaces(latitude: Double, longitude: Double) {
-        viewModel.getPlaces(latitude, longitude)
     }
 
     private fun openMaps(place: Place) {
--- app/src/main/java/com/adyen/android/assignment/ui/places/PlacesViewModel.kt
+++ app/src/main/java/com/adyen/android/assignment/ui/places/PlacesViewModel.kt
@@ -1,5 +1,7 @@
 package com.adyen.android.assignment.ui.places
 
+import android.annotation.SuppressLint
+import android.app.Application
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -9,8 +11,15 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.adyen.android.assignment.api.PlacesService
 import com.adyen.android.assignment.api.VenueRecommendationsQueryBuilder
+import com.adyen.android.assignment.api.model.GeoCode
+import com.adyen.android.assignment.api.model.Location
+import com.adyen.android.assignment.api.model.Main
 import com.adyen.android.assignment.api.model.Place
+import com.google.android.gms.location.LocationRequest
+import com.jintin.fancylocation.LocationData
+import com.jintin.fancylocation.LocationFlow
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -19,18 +28,51 @@ import java.lang.Exception
 import javax.inject.Inject
 
 @HiltViewModel
-class PlacesViewModel @Inject constructor(private val placesService: PlacesService) : ViewModel() {
+@ExperimentalCoroutinesApi
+class PlacesViewModel @Inject constructor(
+    application: Application,
+    private val placesService: PlacesService
+) : ViewModel() {
 
     private val _placesList = MutableStateFlow<List<Place>>(emptyList())
     val placesList: StateFlow<List<Place>> = _placesList
 
-    private val _loading = MutableStateFlow<Boolean>(false)
+    private val _loading = MutableStateFlow(false)
     val loading: StateFlow<Boolean> = _loading
 
-    private val _error = MutableStateFlow<Boolean>(false)
+    private val _error = MutableStateFlow(false)
     val error: StateFlow<Boolean> = _error
 
-    fun getPlaces(latitude: Double, longitude: Double) {
+    private val _locationPermissionGranted = MutableStateFlow(true)
+    val locationPermissionGranted: StateFlow<Boolean> = _locationPermissionGranted
+
+    private val locationRequest =
+        LocationRequest.create()
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+
+    private val locationFlow = LocationFlow(application, locationRequest)
+
+    @SuppressLint("MissingPermission")
+    fun getLocation(){
+        viewModelScope.launch {
+            _loading.value = true
+            locationFlow.get().collect {
+                when (it) {
+                    is LocationData.Success -> {
+                        val latitude = it.location.latitude
+                        val longitude = it.location.longitude
+                        getPlaces(latitude, longitude)
+                    }
+                    is LocationData.Fail -> {
+                        _loading.value = false
+                        _error.value = true
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getPlaces(latitude: Double, longitude: Double) {
         viewModelScope.launch {
             try {
                 _error.value = false
@@ -41,12 +83,16 @@ class PlacesViewModel @Inject constructor(private val placesService: PlacesServi
                 val response = placesService.getVenueRecommendations(queryParams)
                 _loading.value = false
                 _placesList.value = response.results
-            } catch (e: Exception){
+            } catch (e: Exception) {
                 _loading.value = false
                 _error.value = true
             }
 
         }
+    }
+
+    fun onLocationPermissionChange(isGranted: Boolean) {
+        _locationPermissionGranted.value = isGranted
     }
 
 }
